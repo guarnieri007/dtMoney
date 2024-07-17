c03c3694fa843156e25927114ce49cb9edcf77be
Felipe Guarnieri <guarnieri007@hotmail.com>
2024-07-17 20:31:35 -0300

    Table transaction
    Created the components of the table transactions

--- src/pages/trasactions.tsx
+++ src/pages/trasactions.tsx
@@ -1,11 +1,48 @@
 import { Header } from "../components/header";
 import { Summary } from "../components/summary";
+import { PriceHighlight, TransactionsContainer, TrasactionalTable } from "./transactionsStyles";
 
 export function Transactions() {
     return (
         <div>
             <Header />
             <Summary />
+            <TransactionsContainer>
+                <TrasactionalTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    R$12.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                        <tr>
+                            <td>Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                   - R$59,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                        <tr>
+                            <td>Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    R$12.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                    </tbody>
+                </TrasactionalTable>
+            </TransactionsContainer>
         </div>
     )
 }
