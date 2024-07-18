599af79f8ca4da8d50d56798a41fd95bf427c753
Felipe Guarnieri <guarnieri007@hotmail.com>
2024-07-18 01:08:49 -0300

    Started Modal
    Created the new modal structure

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,22 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./headerStyles";
 import logoImg from "../assets/logo.svg"
+import  * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionalModal } from "./newTransactionalModal";
 
 export function Header() {
     return (
         <HeaderContainer>
             <HeaderContent>
                 <img src={logoImg} alt="" />
-                <NewTransactionButton>Nova Transação</NewTransactionButton>
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>Nova Transação</NewTransactionButton>
+                    </Dialog.Trigger>
+                    <NewTransactionalModal>
+                        
+                    </NewTransactionalModal>
+
+                </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
     )
