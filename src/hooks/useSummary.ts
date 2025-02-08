2ae32d104e0b50e33d09f4127bac760e2c85d413
Felipe Guarnieri <guarnieri007@hotmail.com>
2025-02-08 05:23:22 -0300

    Adjusted the math

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -10,8 +10,8 @@ export function useSummary() {
         acc.income += transaction.price
         acc.total += transaction.price
       } else {
-        acc.outcome += transaction.price
-        acc.total += transaction.price
+        acc.outcome -= transaction.price
+        acc.total -= transaction.price
       }
       return acc
     },
